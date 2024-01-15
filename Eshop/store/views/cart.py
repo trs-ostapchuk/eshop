5c56c0d01d709e4b401c139a85d119690127b69b
Taras Ostapchuk <trs.ostp@gmail.com>
2024-01-15 17:23:29 +0100

    Adjust view cart and panel admin

--- Eshop/store/views/cart.py
+++ Eshop/store/views/cart.py
@@ -1,24 +1,14 @@
-from django.db import models
-from store.models.products import Products
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.hashers import check_password
 from store.models.customer import Customer
-import datetime
+from django.views import View
+from store.models.products import Products
 
 
-class Order(models.Model):
-    product = models.ForeignKey(Products,
-                                on_delete=models.CASCADE)
-    customer = models.ForeignKey(Customer,
-                                 on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-    price = models.IntegerField()
-    address = models.CharField(max_length=50, default='', blank=True)
-    phone = models.CharField(max_length=50, default='', blank=True)
-    date = models.DateField(default=datetime.datetime.today)
-    status = models.BooleanField(default=False)
-
-    def placeOrder(self):
-        self.save()
-
-    @staticmethod
-    def get_orders_by_customer(customer_id):
-        return Order.objects.filter(customer=customer_id).order_by('-date')
+class Cart(View):
+    def get(self, request):
+        ids = list(request.session.get('cart').keys())
+        products = Products.get_products_by_id(ids)
+        print(products)
+        return render(request, 'cart.html', {'products': products})
