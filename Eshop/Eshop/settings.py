1ff50af09bb670c508a354e091c6d2cadc25e1cf
Taras Ostapchuk <trs.ostp@gmail.com>
2024-01-15 16:43:03 +0100

    Adjust settings

--- Eshop/Eshop/settings.py
+++ Eshop/Eshop/settings.py
@@ -110,13 +110,17 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
+USE_L10N = True
+
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = "/image/download/"
+MEDIA_ROOT = BASE_DIR
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
